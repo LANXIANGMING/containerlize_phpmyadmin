245f7291be3d4f95f0b6ed7d74363e85b8d4b79d
lanxm <BQyPGGE4T3WUyX6Nwr4J>
2023-01-03 16:28:10 +0800

    init

---
+++ local-nginx_docker-php-mysql-phpmyadmin/.docker/phpmyadmin/config.user.inc.php
@@ -0,0 +1,12 @@
+<?php
+
+
+$cfg['blowfish_secret'] = 'Stress Study~!';
+
+$i = 0;
+
+$i++;
+
+//$cfg['Servers'][$i]['users'] = false;
+
+$cfg['Servers'][$i]['hide_db'] = 'information_schema|performance_schema|mysql|sys';
